a375ff677c2deeffa803904d002575bc8e9d3123
Vladimir Botka <vbotka@gmail.com>
2019-06-01 21:22:27 +0200

    Formatting bool_utils

--- filter_plugins/bool_utils/bool_utils.py
+++ filter_plugins/bool_utils/bool_utils.py
@@ -1,11 +1,14 @@
-def bool_and(h):
-    return all(h)
-def bool_or(h):
-    return any(h)
+def bool_and(l):
+    return all(l)
+
+def bool_or(l):
+    return any(l)
+
 class FilterModule(object):
-    ''' utility filters for operating on list of Boolean '''
+    ''' Ansible filters for operating on list of Boolean '''
+
     def filters(self):
         return {
-            'bool_and' : bool_and
-            ,'bool_or' : bool_or
+            'bool_and' : bool_and,
+            'bool_or' : bool_or
         }
