5c2607006ea3e1261c4385eb6ba8d1d75b6c69e4
Vladimir Botka <vbotka@gmail.com>
2020-01-16 17:14:12 +0100

    Added hash_utils filters
        * Added hash_key, hash_value, hash_tuple filters
        * Examples added and tested OK
        * Lint passed

--- filter_plugins/hash_utils/hash_utils.py
+++ filter_plugins/hash_utils/hash_utils.py
@@ -1,6 +1,15 @@
 # All rights reserved (c) 2019, Vladimir Botka <vbotka@gmail.com>
 # Simplified BSD License, https://opensource.org/licenses/BSD-2-Clause
 
+def hash_to_tuple(h):
+    return h.items()[0]
+
+def hash_key(h):
+    return h.keys()[0]
+
+def hash_value(h):
+    return h.values()[0]
+
 def hash_to_tuples(h):
     return h.items()
 
@@ -15,7 +24,10 @@ class FilterModule(object):
 
     def filters(self):
         return {
-            'hash_to_tuples' : hash_to_tuples
-            ,'hash_keys'     : hash_keys
-            ,'hash_values'   : hash_values
+            'hash_to_tuple': hash_to_tuple,
+            'hash_key': hash_key,
+            'hash_value': hash_value,
+            'hash_to_tuples': hash_to_tuples,
+            'hash_keys': hash_keys,
+            'hash_values': hash_values
         }
