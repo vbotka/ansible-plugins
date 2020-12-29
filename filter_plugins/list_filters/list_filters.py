13e6dde6b72e716de0a50417a6a5e8cba1e9428f
Vladimir Botka <vbotka@gmail.com>
2020-12-29 01:02:01 +0100

    Added filter any2items

--- filter_plugins/list_filters/list_filters.py
+++ filter_plugins/list_filters/list_filters.py
@@ -161,6 +161,194 @@ def lists_mergeby(l1, l2, index):
                 d[elem[index]].update(elem)
     return sorted(d.values(), key=itemgetter(index))
 
+def any2items(x, key='key', override=False):
+    ''' Convert any input to list.
+
+    Example 1:
+
+    - name: No changes to a list
+      debug:
+        msg: "{{ fruits|any2items }}"
+      vars:
+        fruits: [apple, banana, orange]
+
+       gives:
+
+       msg:
+         - apple
+         - banana
+         - orang
+
+    Example 2:
+
+    - name: Convert string to first item in a list
+      debug:
+        msg: "{{ fruits|any2items }}"
+      vars:
+        fruits: 'apple'
+
+       gives:
+
+       msg:
+         - apple
+
+    Example 3:
+
+    - name: Convert None to first item in a list
+      debug:
+        msg: "{{ fruits|any2items }}"
+      vars:
+        fruits: None
+
+       gives:
+
+       msg:
+         - None
+
+    Example 4:
+
+    - name: Convert dictionary where all values are dictionaries to a list
+      debug:
+        msg: "{{ fruits|any2items }}"
+      vars:
+        fruits:
+          apple:
+            color: green
+            size: big
+          banana:
+            color: yellow
+            size: small
+
+       gives:
+
+       msg:
+         - color: green
+           key: apple
+           size: big
+         - color: yellow
+           key: banana
+           size: small
+
+    Example 5:
+
+    - name: Same as the above but change key name
+      debug:
+        msg: "{{ fruits|any2items(key='name') }}"
+      vars:
+        fruits:
+          apple:
+            color: green
+            size: big
+          banana:
+            color: yellow
+            size: small
+
+       gives:
+
+       msg:
+         - color: green
+           name: apple
+           size: big
+         - color: yellow
+           name: banana
+           size: small
+
+    Example 6:
+
+    - name: Convert dictionary where NOT all values are dictionaries
+            to a first item in a list
+      debug:
+        msg: "{{ fruits|any2items }}"
+      vars:
+        fruits:
+          apple:
+            color: green
+            size: big
+          banana:
+            color: yellow
+            size: small
+          orange: ripe
+
+       gives:
+
+       msg:
+         - apple:
+             color: green
+             size: big
+           banana:
+             color: yellow
+             size: small
+           orange: ripe
+
+    Example 7:
+
+    - name: Convert dictionary where NOT all values are dictionaries
+            to a first item in a list
+      debug:
+        msg: "{{ fruits|any2items }}"
+      vars:
+        fruits:
+          apple: green
+          banana: yellow
+          orange: ripe
+
+       gives:
+
+       msg:
+         - apple: green
+           banana: yellow
+           orange: ripe
+
+    Example 8:
+
+    - name: Iterate any data by any2items
+      debug:
+        var: item
+      loop: "{{ [{'a': 1},{'b': 2}]|any2items }}"
+
+    - name: Iterate any data by any2items
+      debug:
+        var: item
+      loop: "{{ {'c': 3}|any2items }}"
+
+    '''
+
+    if not isinstance(key, string_types):
+        raise AnsibleFilterError('Argument key for any2items must be string. %s is %s' %
+                                 (key, type(key)))
+    if not isinstance(override, bool):
+        raise AnsibleFilterError('Argument override for any2items must be boollean. %s is %s' %
+                                 (override, type(override)))
+
+    if isinstance(x, Mapping):
+        keys = list(x.keys())
+        values = list(x.values())
+        if all(isinstance(value, Mapping) for value in values):
+            if (not override) and any(key in list(value.keys()) for value in values):
+                raise AnsibleFilterError('Key %s present in the dictionary.' % (key))
+            else:
+                l =  values
+                for idx, item in enumerate(values):
+                   z = item.copy()
+                   z.update({key: keys[idx]})
+                   l[idx] = z
+        else:
+            l = []
+            l.insert(0, x)
+        return l
+    elif isinstance(x, string_types):
+        l = []
+        l.insert(0, x)
+        return l
+    elif isinstance(x, Sequence):
+        l = list(x)
+        return l
+    else:
+        l = []
+        l.insert(0, x)
+        return l
+
+
 def list_test(l1, l2, index):
     d = defaultdict(dict)
     for l in (l1, l2):
@@ -206,4 +394,5 @@ class FilterModule(object):
             'list_wrapper_comp': list_wrapper_comp,
             'lists_mergeby': lists_mergeby,
             'list_test': list_test,
+            'any2items': any2items,
         }
