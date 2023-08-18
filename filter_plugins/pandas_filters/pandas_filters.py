7c1dc2169fee6ae1b0e0c06eb7df0cb0ffdcf1c7
Vladimir Botka <vbotka@gmail.com>
2023-08-18 22:08:54 +0200

    Add pandas_*. Example tested OK.

--- filter_plugins/pandas_filters/pandas_filters.py
+++ filter_plugins/pandas_filters/pandas_filters.py
@@ -1,16 +1,35 @@
-# All rights reserved (c) 2022, Vladimir Botka <vbotka@gmail.com>
+# All rights reserved (c) 2022-2023, Vladimir Botka <vbotka@gmail.com>
 # Simplified BSD License, https://opensource.org/licenses/BSD-2-Clause
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from pandas.io.json import json_normalize
+import pandas as pd
 
 
 def pandas_json_normalize(d):
-    df = json_normalize(d)
+    df = pd.json_normalize(d)
     l_temp = [df.columns.values.tolist()] + df.values.tolist()
     return(l_temp)
+
+
+def pandas_duplicated(d, subset=None, keep='first'):
+    df = pd.DataFrame(d)
+    d_temp = df.duplicated(subset=subset, keep=keep).to_dict()
+    return(d_temp)
+
+
+def pandas_idx_duplicated(l, keep='first'):
+    idx = pd.Index(l)
+    l_temp = idx.duplicated(keep=keep).tolist()
+    return(l_temp)
+
+
+def pandas_drop_duplicates(d, subset=None, keep='first', inplace=True, ignore_index=False):
+    df = pd.DataFrame(d)
+    df.drop_duplicates(subset=subset, keep=keep, inplace=inplace, ignore_index=ignore_index)
+    d_temp = df.to_dict('records')
+    return(d_temp)
 
 
 class FilterModule(object):
@@ -19,4 +38,7 @@ class FilterModule(object):
     def filters(self):
         return {
             'pandas_json_normalize': pandas_json_normalize,
+            'pandas_duplicated': pandas_duplicated,
+            'pandas_idx_duplicated': pandas_idx_duplicated,
+            'pandas_drop_duplicates': pandas_drop_duplicates,
         }
